330aef223bc60ce83d3cd74219aa7413b726e717
jordigh <jgutierrezhermoso@newrelic.com>
2022-12-07 13:36:23 -0500

    NEWRELIC-5570: lint index.js

--- code-level-metrics/express/index.js
+++ code-level-metrics/express/index.js
@@ -1,21 +1,25 @@
+/*
+ * Copyright 2022 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 
 const express = require('express')
 const app = express()
 const { PORT = '3000', HOST = 'localhost' } = process.env
 
-app.listen(PORT, HOST, function() {
+app.listen(PORT, HOST, function () {
   const addr = this.address()
   const host = addr.family === 'IPv6' ? `[${addr.address}]` : addr.address
   console.log('Server started at http://%s:%s', host, addr.port)
 })
 
-
 app.get('/named-mw', function namedMiddlweare(_req, res) {
   res.send('This is a named middleware handler')
 })
 
-app.get('/anon', function(_req, res) {
+app.get('/anon', function (_req, res) {
   res.send('anonymous mw handler')
 })
 
@@ -27,8 +31,11 @@ function mw4(_req, _res, next) {
   next()
 }
 
-const handler = function(_req, res) {
+const handler = function (_req, res) {
   res.send('phew, that was a lot of hops')
 }
 
+// eslint-disable-next-line
 app.get('/chained', function mw1(_req, _res, next) { next() }, function(_req, _res, next) { next() }, (_req, _res, next) => { next() }, mw4, handler)
+// The above is deliberately ugly and in one line, with named,
+// anonymous, and arrow functions all in one big mess.
