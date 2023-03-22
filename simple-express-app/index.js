ea00d017f390aaecf03670e6d01230fec4dedb9a
Bob Evans <robert.evans25@gmail.com>
2023-03-22 17:15:03 -0400

    added aws sdk v3 test

--- simple-express-app/index.js
+++ simple-express-app/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const { PORT = 3000 } = process.env
+const lib = require('@aws-sdk/client-sns')
+const sns = new lib.SNSClient()
 
 app.get('/named-route', (req, res) => {
   res.send('hi')
+})
+
+app.get('/sns', async (req, res) => {
+  const cmd = new lib.ListTopicsCommand({})
+  const result = await sns.send(cmd)
+  res.send(result)
+
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
