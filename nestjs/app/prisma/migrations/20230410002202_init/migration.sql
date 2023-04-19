c5fe42f3b26970494dcb7dd3d585de1d1e6a553d
Jordi Gutiérrez Hermoso <jgutierrezhermoso@newrelic.com>
2023-04-19 15:26:57 -0400

    NEWRELIC-7888: (nestjs) add a Nest.js example app
    This is a basic Nest.js app using Prisma for the database and Winston
    for logging. It exercises several features of Nest.js like
    interceptors and guards. Like other examples here, it can be run in
    Docker.
    
    Optionally, via  NES_USE_FASTIFY envvar, it may use Fastify, but it
    defaults to Express, as that's Nest.js's default.

---
+++ nestjs/app/prisma/migrations/20230410002202_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cat" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "breed" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Cat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Cat" ADD CONSTRAINT "Cat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ nestjs/app/src/app.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { CatService } from './cat.service';
+import { PrismaService } from './prisma.service';
+
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService, UserService, CatService, PrismaService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
