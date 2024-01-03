eb3365d15bc3b98439a8a317982d46786e050667
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2024-01-03 14:16:17 -0600

    Add chat completion invoke modal requests

---
+++ aws-bedrock-app/README.md
@@ -0,0 +1,23 @@
+# Sample AWS Bedrock Application
+
+## Getting started
+**Note**: This application requires the use of Node.js v20+.
+
+ 1. Clone or fork this repository.
+
+ 1. Install dependencies and run application
+
+```sh
+npm ci
+cp .env.sample .env
+# Fill out `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` in .env and save 
+npm start
+```
+
+ 1. Make requests to application.
+
+ ```sh
+# make a request to chat completions - provide a model to use or the default amazon-titan model will be used
+# model options are: amazon-titan, anthropic, ai21, cohere
+curl -XPOST -H 'Content-Type: application/json' http://localhost:3000/chat-completion -d '{"message":"How much would could a woodchuck chuck if a woodchuck could chuck wood?", "model": "amazon-titan"}'
+```
---
+++ aws-bedrock-app/index.js
@@ -0,0 +1,95 @@
+'use strict'
+require('dotenv').config()
+const fastify = require('fastify')({ logger: true })
+const { PORT: port = 3000, HOST: host = '127.0.0.1' } = process.env
+const {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+  InvokeModelWithResponseStreamCommand
+} = require('@aws-sdk/client-bedrock-runtime')
+
+const client = new BedrockRuntimeClient({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+})
+
+fastify.listen({ host, port }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
+
+fastify.post('/chat-completion', async (request, reply) => {
+  const { message = 'Say this is a test', model = 'amazon-titan' } = request.body || {};
+
+  // Model configurations
+  const modelConfigurations = {
+    // amazon titan text
+    'amazon-titan': {
+      modelId: 'amazon.titan-text-express-v1',
+      body: { inputText: message },
+    },
+    // anthropic claude
+    'anthropic': {
+      modelId: 'anthropic.claude-v2',
+      body: {
+        prompt: `\n\nHuman: ${message}\n\nAssistant:`,
+        max_tokens_to_sample: 200,
+      },
+    },
+    // ai21 labs jurassic 2
+    'ai21': {
+      modelId: 'ai21.j2-mid-v1',
+      body: { prompt: message },
+    },
+    // cohere
+    'cohere': {
+      modelId: 'cohere.command-text-v14',
+      body: { prompt: message },
+    },
+  };
+
+  const modelConfig = modelConfigurations[model];
+  if (!modelConfig) {
+    return reply.code(400).send({ error: 'Invalid model' });
+  }
+
+  const command = new InvokeModelCommand({
+    body: JSON.stringify(modelConfig.body),
+    modelId: modelConfig.modelId,
+    contentType: 'application/json',
+    accept: 'application/json',
+  });
+
+  try {
+    const response = await client.send(command);
+    const resBody = new TextDecoder('utf-8').decode(response.body);
+    const parsedResBody = JSON.parse(resBody);
+
+    let outputText;
+
+    switch (model) {
+      case 'amazon-titan':
+        outputText = parsedResBody.results[0].outputText;
+        break;
+      case 'anthropic':
+        outputText = parsedResBody.completion;
+        break;
+      case 'ai21':
+        outputText = parsedResBody.completions[0].data.text;
+        break;
+      case 'cohere':
+        outputText = parsedResBody.generations[0].text;
+        break;
+    }
+
+    return reply.send({"requestId": response.$metadata.requestId, outputText});
+  } catch (error) {
+    console.error(error);
+    return reply.code(500).send({ error: error });
+  }
+});
