50f185ebf1754ad6ff782716850c85c845c83b6d
Bob Evans <robert.evans25@gmail.com>
2024-01-12 12:37:45 -0500

    refactor: consolidated prompt payload in requests.js, upgraded agent, fixed issues when testing out all the endpoints

--- aws-bedrock-app/index.js
+++ aws-bedrock-app/index.js
@@ -8,6 +8,7 @@ const {
   InvokeModelWithResponseStreamCommand
 } = require('@aws-sdk/client-bedrock-runtime');
 
+const requests = require('./requests')
 const responses = new Map();
 
 const client = new BedrockRuntimeClient({
@@ -28,41 +29,16 @@ fastify.listen({ host, port }, function (err, address) {
 fastify.post('/chat-completion', async (request, reply) => {
   const { message = 'Say this is a test', model = 'amazon-titan' } = request.body || {};
 
-  // Model configurations
-  const modelConfigurations = {
-    // amazon titan text
-    'amazon-titan': {
-      modelId: 'amazon.titan-text-express-v1',
-      body: { inputText: message },
-    },
-    // anthropic claude
-    'anthropic': {
-      modelId: 'anthropic.claude-v2',
-      body: {
-        prompt: `\n\nHuman: ${message}\n\nAssistant:`,
-        max_tokens_to_sample: 200,
-      },
-    },
-    // ai21 labs jurassic 2
-    'ai21': {
-      modelId: 'ai21.j2-ultra-v1',
-      body: { prompt: message },
-    },
-    // cohere
-    'cohere': {
-      modelId: 'cohere.command-text-v14',
-      body: { prompt: message },
-    },
-  };
-
-  const modelConfig = modelConfigurations[model];
+  const modelConfig = requests[model]
   if (!modelConfig) {
     return reply.code(400).send({ error: 'Invalid model' });
   }
 
+  const data = modelConfig(message)
+
   const command = new InvokeModelCommand({
-    body: JSON.stringify(modelConfig.body),
-    modelId: modelConfig.modelId,
+    body: JSON.stringify(data.body),
+    modelId: data.modelId,
     contentType: 'application/json',
     accept: 'application/json',
   });
@@ -87,47 +63,35 @@ fastify.post('/chat-completion', async (request, reply) => {
       case 'cohere':
         outputText = parsedResBody.generations[0].text;
         break;
+      case 'meta': 
+        outputText = parsedResBody.generation
+        break;
     }
 
-    return reply.send({"requestId": response.$metadata.requestId, outputText});
+    const { requestId } = response.$metadata
+    const ids = newrelic.getLlmMessageIds({ responseId: requestId })
+    responses.set(requestId, ids)
+
+    return reply.send({requestId, outputText});
   } catch (error) {
-    return reply.code(500).send({ error: error });
+    const code = error?.$metadata?.httpStatusCode || 500
+    return reply.code(code).send({ error });
   }
 });
 
 fastify.post('/chat-completion-stream', async(request, reply) => {
   const { message = 'Say this is a test', model = 'amazon-titan' } = request.body || {};
 
-  // Model configurations
-  const modelConfigurations = {
-    // amazon titan text
-    'amazon-titan': {
-      modelId: 'amazon.titan-text-express-v1',
-      body: { inputText: message },
-    },
-    // anthropic claude
-    'anthropic': {
-      modelId: 'anthropic.claude-v2',
-      body: {
-        prompt: `\n\nHuman: ${message}\n\nAssistant:`,
-        max_tokens_to_sample: 200,
-      },
-    },
-    // cohere
-    'cohere': {
-      modelId: 'cohere.command-text-v14',
-      body: { prompt: message },
-    },
-  };
-
-  const modelConfig = modelConfigurations[model];
+  const modelConfig = requests[model]
   if (!modelConfig) {
     return reply.code(400).send({ error: 'Invalid model' });
   }
 
+  const data = modelConfig(message)
+
   const command = new InvokeModelWithResponseStreamCommand({
-    body: JSON.stringify(modelConfig.body),
-    modelId: modelConfig.modelId,
+    body: JSON.stringify(data.body),
+    modelId: data.modelId,
     contentType: 'application/json',
     accept: 'application/json',
   });
@@ -148,18 +112,25 @@ fastify.post('/chat-completion-stream', async(request, reply) => {
     
     return reply;
   } catch (error) {
-    return reply.code(500).send({ error: error });
+    const code = error?.$metadata?.httpStatusCode || 500
+    return reply.code(code).send({ error });
   }
 })
 
 fastify.post('/embedding', async (request, reply) => {
   const { message = 'Test embedding', model = 'amazon-titan-embed' } = request.body || {}
+  
+  const modelConfig = requests[model]
+
+  if (!modelConfig) {
+    return reply.code(400).send({ error: 'Invalid model' });
+  }
+
+  const data = modelConfig(message)
 
   const prompt = {
-    body: JSON.stringify({
-      inputText: message,
-    }),
-    modelId: 'amazon.titan-embed-text-v1',
+    body: JSON.stringify(data.body),
+    modelId: data.modelId, 
     contentType: 'application/json',
     accept: 'application/json'
   }
@@ -174,7 +145,8 @@ fastify.post('/embedding', async (request, reply) => {
 
     return reply.send({"requestId": response.$metadata.requestId, embedding});
   } catch (error) {
-    return reply.code(500).send({ error: error });
+    const code = error?.$metadata?.httpStatusCode || 500
+    return reply.code(code).send({ error });
   }
 })
 
---
+++ aws-bedrock-app/requests.js
@@ -0,0 +1,58 @@
+'use strict'
+
+module.exports = {
+  'amazon-titan': (message) => ({
+    modelId: 'amazon.titan-text-express-v1',
+    body: { inputText: message },
+  }),
+  'amazon-titan-embed': (message) => ({
+    modelId: 'amazon.titan-embed-text-v1',
+    body: { inputText: message },
+  }),
+  'anthropic': (message) => ({
+    modelId: 'anthropic.claude-v2',
+    body: {
+      prompt: `\n\nHuman: ${message}\n\nAssistant:`,
+      max_tokens_to_sample: 200,
+    },
+  }),
+  'ai21': (message) => ({
+    modelId: 'ai21.j2-ultra-v1',
+    body: { prompt: message },
+  }),
+  'cohere': (message) => ({
+    modelId: 'cohere.command-text-v14',
+    body: { prompt: message },
+  }),
+  'cohere-embed': (message) => ({
+    modelId: 'cohere.embed-english-v3',
+    body: { texts: message.split(' '), input_type: 'search_document' },
+  }),
+  'meta': (message) => ({
+    modelId: 'meta.llama2-13b-chat-v1',
+    body: { prompt: message },
+  }),
+  'amazon-invalid': (message) => ({
+    modelId: 'amazon.titan-text-express-v1',
+    body: { message },
+  }),
+  'anthropic-invalid': (message) => ({
+    modelId: 'anthropic.claude-v2',
+    body: {
+      inputText: `\n\nHuman: ${message}\n\nAssistant:`,
+      max_tokens_to_sample: 200,
+    },
+  }),
+  'ai21-invalid': (message) => ({
+    modelId: 'ai21.j2-ultra-v1',
+    body: { inputText: message },
+  }),
+  'cohere-invalid': (message) => ({
+    modelId: 'cohere.command-text-v14',
+    body: { inputText: message },
+  }),
+  'meta-invalid': (message) => ({
+    modelId: 'meta.llama2-13b-chat-v1',
+    body: { inputText: message },
+  }),
+}
