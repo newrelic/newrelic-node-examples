f73903e08aed130713ca511c3f0660de3c24ad6c
Amy Chisholm <amychisholm2003@gmail.com>
2024-07-17 06:55:13 -0700

    feat: added segments app

---
+++ custom-instrumentation/segments/README.md
@@ -0,0 +1,34 @@
+# Sample Custom Segments Application
+
+This example provides an application with custom segments. Segments are a way to further break apart the different steps of a transaction and see the stats about that specific piece of code.
+
+## Getting Started
+
+1. Clone or fork this repository.
+2. Navigate to this example's sub directory
+   ```
+   cd newrelic-node-examples/custom-instrumentation/segments
+   ```
+3. Install dependencies and run application.
+   ```
+   npm install
+   cp env.sample .env
+   # Fill out `NEW_RELIC_LICENSE_KEY` in .env and save 
+   # Start the application
+   npm start
+   ```
+4. The application will automatically start running background transactions with custom segments inside them. Modify index.js to include/omit more transactions/segments.
+
+## Exploring Telemetry
+
+After sending a few requests, navigate to your application in APM & Services. Locate the 'Example Custom Segments App' service and then select 'Distirbuted tracing' on the left side of your screen. Then, select one of the trace groups that will contain your custom segment and selct a single trace. If you expand the transaction (click 'Show in-process spans'), you should see your custom segment. Click on it to see its details.
+
+![1721223889308](image/README/1721223889308.png)
+
+To query your segment data, you can run a query like the following:›
+
+```NRQL
+SELECT count(*) FROM Span FACET name WHERE name = 'myCustomSegment'
+```
+
+![1721224323000](image/README/1721224323000.png)
---
+++ custom-instrumentation/segments/index.js
@@ -0,0 +1,213 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+const newrelic = require('newrelic')
+
+//example1-callbacks
+function callbackSegment() {
+    return new Promise((resolve) => {
+        /*
+        * We'll stub out an async task that runs as part of monitoring a segment
+        */
+        function myAsyncTask(callback) {
+            const sleep = new Promise((resolve) => {
+                setTimeout(resolve, 1)
+            })
+            sleep.then(() => {
+                callback(null, 'callback segment')
+            })
+        }
+
+        /*
+         * Then we stub out the task that handles that task's result,
+         * to show how the result is passed throughthe segment handler
+         */
+        function myNextTask(phrase, callback) {
+            callback(null, `${phrase} completed!`)
+        }
+
+        /*
+         * This task will be run as its own segment within our transaction handler
+         */
+        function someTask(callback) {
+            myAsyncTask(function firstCb(err1, result) {
+                if (err1) {
+                    return callback(err1)
+                }
+
+                myNextTask(result, function secondCb(err2, output) {
+                    callback(err2, output)
+                })
+            })
+        }
+
+        // Segments can only be created inside of transactions. They could be automatically
+        // generated HTTP transactions or custom transactions.
+        newrelic.startBackgroundTransaction('bg-tx', function transHandler() {
+            const tx = newrelic.getTransaction()
+
+            // `startSegment()` takes a segment name, a boolean if a metric should be
+            // created for this segment, the handler function, and an optional callback.
+            // The handler is the function that will be wrapped with the new segment. When
+            // a callback is provided, the segment timing will end when the callback is
+            // called.
+
+            newrelic.startSegment('myCallbackSegment', false, someTask, function cb(err, output) {
+                // Handle the error and output as appropriate.
+                tx.end()
+                resolve(output)
+            })
+        })
+    })
+}
+
+//example2-promises
+function promiseSegment() {
+    return new Promise((resolve) => {
+        /*
+         * We'll stub out an async task that runs as part of monitoring a segment
+         */
+        async function myAsyncTask() {
+            await new Promise((resolve) => {
+                setTimeout(resolve, 1)
+            })
+            return 'promise segment'
+        }
+
+        /*
+         * Then we stub out the task that handles that task's result,
+         * to show how the result is passed throughthe segment handler
+         *
+         */
+        function myNextTask(phrase) {
+            return `${phrase} completed!`
+        }
+
+        /*
+         * This task will be run as its own segment within our transaction handler
+         */
+        function someTask() {
+            return myAsyncTask().then(function thenNext(result) {
+                return myNextTask(result)
+            })
+        }
+
+        // Segments can only be created inside of transactions. They could be automatically
+        // generated HTTP transactions or custom transactions.
+        newrelic.startBackgroundTransaction('bg-tx', function transHandler() {
+            const tx = newrelic.getTransaction()
+
+            // `startSegment()` takes a segment name, a boolean if a metric should be
+            // created for this segment, the handler function, and an optional callback.
+            // The handler is the function that will be wrapped with the new segment. If
+            // a promise is returned from the handler, the segment's ending will be tied
+            // to that promise resolving or rejecting.
+
+            return newrelic.startSegment('myPromiseSegment', false, someTask).then(function thenAfter(output) {
+                tx.end()
+                resolve(output)
+            })
+        })
+    })
+}
+
+//example3-async
+function asyncSegment() {
+    return new Promise((resolve) => {
+        /*
+         * We'll stub out an async task that runs as part of monitoring a segment
+         */
+        async function myAsyncTask() {
+            await new Promise((resolve) => {
+                setTimeout(resolve, 1)
+            })
+            return 'async segment'
+        }
+
+        /*
+         * Then we stub out the task that handles that task's result,
+         * to show how the result is passed throughthe segment handler.
+         */
+        async function myNextTask(phrase) {
+            await new Promise((resolve) => {
+                setTimeout(resolve, 1)
+            })
+            return `${phrase} completed!`
+        }
+
+        /**
+         * This task will be run as its own segment within our transaction handler
+         */
+        async function someTask() {
+            const result = await myAsyncTask()
+            return await myNextTask(result)
+        }
+
+        // Segments can only be created inside of transactions. They could be automatically
+        // generated HTTP transactions or custom transactions.
+        newrelic.startBackgroundTransaction('bg-tx', async function transHandler() {
+            // `startSegment()` takes a segment name, a boolean if a metric should be
+            // created for this segment, the handler function, and an optional callback.
+            // The handler is the function that will be wrapped with the new segment.
+            // Since `async` functions just return a promise, they are covered just the
+            // same as the promise example.
+
+            const output = await newrelic.startSegment('myAsyncSegment', false, someTask)
+            resolve(output)
+        })
+    })
+}
+
+//example4-sync-assign
+function syncAssignSegment() {
+    return new Promise((resolve) => {
+        /*
+        * We'll stub out an async task that runs as part of monitoring a segment.
+        */
+        function mySyncTask() {
+            return 'sync assign segment'
+        }
+
+        /*
+         * Then we stub out the task that handles that task's result,
+         * to show how the result is passed throughthe segment handler.
+         *
+         * @param greetings
+         */
+        function myNextTask(phrase) {
+            return `${phrase} completed!`
+        }
+
+        /*
+         * This task will be run as its own segment within our transaction handler
+         */
+        function someTask() {
+            const result = mySyncTask()
+            return myNextTask(result)
+        }
+
+        // Segments can only be created inside of transactions. They could be automatically
+        // generated HTTP transactions or custom transactions.
+        newrelic.startBackgroundTransaction('bg-tx', function transHandler() {
+            // `startSegment()` takes a segment name, a boolean if a metric should be
+            // created for this segment, the handler function, and an optional callback.
+            // The handler is the function that will be wrapped with the new segment.
+
+            const output = newrelic.startSegment('mySyncAssignSegment', false, function timedFunction() {
+                return someTask()
+            })
+            resolve(output) // "hello world, it's me!"
+        })
+    })
+}
+
+async function main() {
+    const data = await Promise.all([callbackSegment(), promiseSegment(), asyncSegment(), syncAssignSegment()])
+    console.log(data)
+    newrelic.shutdown({ collectPendingData: true }, () => process.exit(0))
+}
+
+main()
