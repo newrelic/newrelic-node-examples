44dbdbb8cd4def4cd63d00c85649b08578713370
Amy Chisholm <achisholm@newrelic.com>
2024-08-13 08:15:16 -0700

    feat: instrument example (#280)
    Co-authored-by: James Sumners <jsumners@newrelic.com>

---
+++ custom-instrumentation/instrument/README.md
@@ -0,0 +1,44 @@
+# Example instrumentation of a basic app
+
+This example application shows you how to use the [newrelic.instrument](https://newrelic.github.io/node-newrelic/API.html#instrument) and associated [shim](https://newrelic.github.io/node-newrelic/Shim.html) API. It instruments a simple module, a rudimentary job queue (`jobQueue`), and runs a series of basic jobs.
+
+## Getting Started
+
+1. Clone or fork this repository.
+2. Navigate to this example's sub directory
+   ```
+   cd newrelic-node-examples/custom-instrumentation/instrument
+   ```
+3. Install dependencies and run application.
+   ```
+   npm install
+   cp env.sample .env
+   # Fill out `NEW_RELIC_LICENSE_KEY` in .env and save 
+   # Start the application
+   npm start
+   ```
+4. The app will automatically start adding example jobs to a queue and run them. You should see the following in the console when the instrumentation takes place.
+   ```
+   [NEWRELIC] instrumenting 'job-queue'
+   Callback job done
+   Promise job done
+   ```
+
+## Exploring Telemetry
+
+1. After a few minutes, you should be able to see `job-queue` instrumented in New Relic. From the dashboard, navigate to 'APM & Services' and then select the 'Example Job Queue App' entity.
+2. Then select 'Distributed tracing'. You should see the trace groups `firstTransacation`, `secondTransaction`, and `thirdTransaction`. Inside these groups will be our custom instrumentation. Select any trace group and then select a single trace.
+3. Under `firstTransaction` or `secondTransaction`, toggle 'Show in-process spans' and you will see 'scheduleJob - job'; this shows the name of which job was recorded. Under `thirdTransaction`, you will see `queue.runJobs` instrumented as 'runJobs'.
+
+   ![1722869774240](./image/README/1722869774240.png)
+
+   ![1722869857647](./image/README/1722869857647.png)
+
+## Description
+
+This application consists of the following files:
+
+* `index.js`: a simple app that schedules jobs and runs them
+* `job-queue.js`: an example module that provides a queue class that you can use to run and schedule jobs
+* `instrumentation.js`: all of the New Relic instrumentation is in here; the `npm start` command makes sure this module is loaded first
+* `newrelic.js`: a basic, sample New Relic configurartion
---
+++ custom-instrumentation/instrument/index.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const newrelic = require('newrelic')
+const Queue = require('./job-queue')
+
+function exampleJob() {
+    // Do whatever work you want here - this is just a placeholder
+    return 'job done';
+}
+
+function cbJob(cb) {
+    const result = exampleJob();
+    return cb('Callback ' + result);
+}
+
+async function promiseJob() {
+    return new Promise((resolve, reject) => {
+        try {
+            const result = exampleJob()
+            resolve('Promise ' + result)
+        }
+        catch (error) {
+            reject(error)
+        }
+    })
+}
+
+function main() {
+    const queue = new Queue()
+
+    // We will be creating our transactions with startBackgroundTransaction
+    // because this application does not utilize frameworks New Relic already
+    // instruments. Thus, transactions are not automatically created for use.
+    // If you are already operating within an instrumented framework, you may
+    // omit the startBackgroundTransaction wrapper.
+
+    // Without instrumentation, executing this code will cause 'firstTransaction'
+    // to be the active transaction in both 'firstJob' and 'secondJob'. This is
+    // not the intended behavior. If we instrument queue.scheduleJob, the 
+    // functions will be correctly placed within their respective transactions.
+    newrelic.startBackgroundTransaction('firstTransaction', function () {
+        const transaction = newrelic.getTransaction()
+        queue.scheduleJob(async function firstJob() {
+            // Do some work - for example, this waits for a promise job to complete
+            const result = await promiseJob()
+            console.log(result)
+            transaction.end()
+        })
+    })
+
+    newrelic.startBackgroundTransaction('secondTransaction', function () {
+        const transaction = newrelic.getTransaction()
+        queue.scheduleJob(function secondJob() {
+            // Do some work - for example, this waits for a callback job to complete
+            cbJob(function cb(result) {
+                console.log(result)
+                transaction.end()
+            })
+        })
+    })
+
+    // Wait for the jobs to be added to the queue and then run them
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    sleep(1000).then(() =>
+        newrelic.startBackgroundTransaction('thirdTransaction', function () {
+            const transaction = newrelic.getTransaction()
+            queue.runJobs()
+            transaction.end()
+            // Finally shutdown the agent so it properly flushes all data
+            newrelic.shutdown({ collectPendingData: true }, () => process.exit(0))
+        })
+    )
+}
+
+main()
