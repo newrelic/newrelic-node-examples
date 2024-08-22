f205766d83d4a9b5749b29fa935f51e0688c8d42
Bob Evans <robert.evans25@gmail.com>
2024-08-22 15:52:19 -0400

    feat: Added example of using startWebTransaction

---
+++ custom-instrumentation/start-web-transaction/client.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const io = require('socket.io-client')
+
+const socket = io.connect('http://localhost:3000', { reconnect: true })
+let responses = 0
+
+socket.on('pong', () => {
+  responses += 1
+  console.log('ping pong success')
+})
+
+socket.on('message-received', (data) => {
+  responses += 1
+  console.log(`message received: ${JSON.stringify(data)}`)
+})
+
+socket.on('update-done', (data) => {
+  responses += 1
+  console.log(`update received: ${JSON.stringify(data)}`)
+})
+
+socket.emit('ping')
+
+for (let i = 1; i < 21; i++) {
+  socket.emit('update', { name: `Message ${i}`, msg: 'Hello world' })
+  socket.emit('new-message', `this is message ${i}`)
+}
+
+setInterval(() => {
+  if (responses === 41) {
+    console.log('done processing events from server')
+    // eslint-disable-next-line no-process-exit
+    process.exit(0)
+  }
+}, 500)
---
+++ custom-instrumentation/start-web-transaction/index.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+const newrelic = require('newrelic')
+const app = require('http').createServer()
+const io = require('socket.io')(app)
+const randomNum = () => Math.ceil(Math.random() * 100)
+
+function updateChatWindow(data, cb) {
+  newrelic.startSegment('updateChatWindow', true, function handler() {
+    setTimeout(() => {
+      console.log('got update data', data)
+      cb(null, data)
+    }, randomNum())
+  })
+}
+
+function addMessageToChat(data, cb) {
+  newrelic.startSegment('addMessageToChat', true, function handler() {
+    setTimeout(() => {
+      console.log('got add message data', data)
+      cb(null, data)
+    }, randomNum())
+  })
+}
+
+function onPing() {
+  const socket = this
+  newrelic.startWebTransaction('/websocket/ping', function transactionHandler() {
+    // transaction is ended automatically after synchronous operations
+    socket.emit('pong')
+  })
+}
+
+function onUpdate(data) {
+  const socket = this
+  newrelic.startWebTransaction('/websocket/update', function transactionHandler() {
+    // Must manually end transaction after the callback is invoked
+    const transaction = newrelic.getTransaction()
+    updateChatWindow(data, function chatWinCb(err, results) {
+      socket.emit('update-done', results)
+      transaction.end()
+    })
+  })
+}
+
+function onNewMsg(data) {
+  const socket = this
+  newrelic.startWebTransaction('/websocket/new-message', function transactionHandler() {
+    // Returning a promise
+    return new Promise((resolve) => {
+      addMessageToChat(data, function addMsgCb(err, results) {
+        socket.emit('message-received', results)
+        resolve()
+      })
+    })
+  })
+}
+
+io.on('connection', function onConnect(socket) {
+  socket.on('ping', onPing)
+  socket.on('update', onUpdate)
+  socket.on('new-message', onNewMsg)
+})
+
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
