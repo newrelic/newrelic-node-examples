f27363571e0adc8075f6536005c1e9c3ad6f8b9f
mrickard <maurice@mauricerickard.com>
2025-03-31 14:22:00 -0400

    chore: Added HTTP methods to the Azure Functions HTTP example
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- azure-functions/src/functions/HttpExample.js
+++ azure-functions/src/functions/HttpExample.js
@@ -1,13 +1,29 @@
 const { app } = require('@azure/functions');
 
+// HTTP trigger handling multiple methods
 app.http('HttpExample', {
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-        debugger
-        const name = request.query.get('name') || await request.text() || 'world';
+        let name
+        switch(request.method) {
+            case 'GET':
+                name = request.query.get('name')
+                break
+            case 'POST':
+            case 'PUT':
+            case 'PATCH':
+                const input = await request.text()
+                name = JSON.parse(input).name
+                break
+            case 'DELETE':
+                name = request.query.get('name')
+        }  
+        context.log(`http function processed request for url "${request.url}"`)
+        
+        if (!name)
+          name = 'World'
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello, ${name}! You sent a ${request.method} request.` }
     }
 });
