4c6ddb888a34201e4ef680555bc1f759ddaeeb7d
Amy Chisholm <achisholm@newrelic.com>
2025-04-21 10:54:38 -0700

    Only GET for streaming

--- azure-functions/src/functions/HttpStreaming.js
+++ azure-functions/src/functions/HttpStreaming.js
@@ -1,14 +1,14 @@
 const newrelic = require('newrelic');
 const { app } = require('@azure/functions');
-const { Writable, Transform } = require('stream');
-const { createReadStream, createWriteStream } = require('fs');
+const { Transform } = require('stream');
+const { createReadStream } = require('fs');
 
 // Enable streaming
 app.setup({ enableHttpStream: true });
 
 // HTTP trigger with streaming support
 app.http('HttpStreaming', {
-    methods: ['POST', 'GET'],
+    methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
@@ -21,11 +21,6 @@ app.http('HttpStreaming', {
 
             const body = createReadStream('helloWorld.txt').pipe(delayStream);
             return { body };
-        }
-        if (request.method === 'POST') {
-            const writeStream = createWriteStream('helloWorld.txt');
-            await request.body.pipeTo(Writable.toWeb(writeStream));
-            return { body: 'Done!' };
         }
     }
 });
