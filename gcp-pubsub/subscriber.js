778074b2bde812737a85d304901a095b93e37f27
Amy Chisholm <achisholm@newrelic.com>
2025-04-21 08:11:57 -0700

    feat: GCP PubSub example (#316)

---
+++ gcp-pubsub/subscriber.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const newrelic = require('newrelic')
+const { PubSub } = require('@google-cloud/pubsub')
+
+async function startSubscriber() {
+    // Assumes Google Cloud credentials are set up via this example:
+    // https://cloud.google.com/pubsub/docs/publish-receive-messages-client-library#node.js
+    const subscriber = new PubSub({ enableOpenTelemetryTracing: true })
+    const subscriptionName = process.env.GCP_SUBSCRIPTION
+    const subscription = subscriber.subscription(subscriptionName)
+    console.log(`Listening for messages on ${subscriptionName}`)
+    subscription.on('message', message => {
+        console.log(`Received message: ${message.data}`)
+        message.ack()
+    })
+}
+
+async function main() {
+    if (newrelic.agent.collector.isConnected() === false) {
+        await new Promise((resolve) => {
+            newrelic.agent.on('connected', resolve)
+        })
+    }
+    await startSubscriber()
+}
+
+main()
