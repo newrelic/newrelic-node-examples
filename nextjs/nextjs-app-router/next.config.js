77a101d8e0ae434ca1e22265031cc09e212641e1
Maurice Rickard <maurice@mauricerickard.com>
2025-04-29 14:42:21 -0400

    chore: Added notes on instrumenting NextJs client pages and Turbopack (#327)
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- nextjs/nextjs-app-router/next.config.js
+++ nextjs/nextjs-app-router/next.config.js
@@ -3,13 +3,19 @@
 const nrExternals = require('newrelic/load-externals')
 
 module.exports = {
+  // For use with Next 14 and earlier:
   experimental: {
     // Without this setting, the Next.js compilation step will routinely
     // try to import files such as `LICENSE` from the `newrelic` module.
     // See https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages.
     serverComponentsExternalPackages: ['newrelic']
   },
-
+  
+  // In Next15, server external packages are no longer experimental, this property is renamed serverExternalPackages. 
+  // `newrelic` is already automatically opted out of bundling, so does not need to be defined here. 
+  // See https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages
+  // serverExternalPackages: ['otherDependency']
+  
   // In order for newrelic to effectively instrument a Next.js application,
   // the modules that newrelic supports should not be mangled by webpack. Thus,
   // we need to "externalize" all of the modules that newrelic supports.
@@ -17,4 +23,12 @@ module.exports = {
     nrExternals(config)
     return config
   }
+  
+  // Turbopack: NextJs offers the Turbopack incremental bundler as a replacement for webpack. 
+  // Its configuration requires a minimal change: 
+  //
+  // turbopack: (config) => {
+  //   nrExternals(config)
+  //   return config
+  // }
 }
