d02348f55bf430fe049f1d76b4f089733035050e
Amy Chisholm <achisholm@newrelic.com>
2025-04-30 08:31:36 -0700

    feat: GraphQL OTel bridge example app (#325)

---
+++ opentelemetry-graphql/client.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const newrelic = require('newrelic')
+const url = require('url')
+const http = require('http')
+// Construct a schema, using GraphQL schema language
+
+const source = `
+query {
+  books {
+    name
+    authors {
+      name
+      address {
+        country
+      }
+    }
+  }
+}
+`
+
+makeRequest(source).then(console.log)
+
+function makeRequest(query) {
+  return new Promise((resolve, reject) => {
+    const parsedUrl = new url.URL('http://localhost:4000/graphql')
+    const options = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: parsedUrl.pathname,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    newrelic.startBackgroundTransaction('send-request', () => {
+      const txn = newrelic.getTransaction()
+      const req = http.request(options, (res) => {
+        const data = []
+        res.on('data', (chunk) => data.push(chunk))
+        res.on('end', () => {
+          resolve(data.toString())
+        })
+        res.on('error', (err) => {
+          reject(err)
+        })
+      })
+
+      req.write(JSON.stringify({ query }))
+      req.end()
+      txn.end()
+      newrelic.shutdown({ collectPendingData: true }, () => process.exit(0))
+    })
+  })
+}
