8b5ff40069eec14a7a435499f2b3bad05c3977af
Amy Chisholm <achisholm@newrelic.com>
2025-05-07 09:04:47 -0700

    wip: gcp cloud run

---
+++ gcp-cloud-run/README.md
@@ -0,0 +1,5 @@
+# GCP Cloud Run example app
+
+## Running the Example App
+
+1. Set up your Google Cloud project by following their set up guide [here](https://cloud.google.com/run/docs/quickstarts/build-and-deploy/deploy-nodejs-service?hl=en).
---
+++ gcp-cloud-run/helloworld/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
