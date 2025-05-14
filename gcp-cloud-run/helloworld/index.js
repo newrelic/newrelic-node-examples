9a502f5ae7504cc5337646e07e8387f5193961d2
Amy Chisholm <achisholm@newrelic.com>
2025-05-14 08:23:42 -0700

    feat: GCP Cloud Run example app (#332)

---
+++ gcp-cloud-run/helloworld/index.js
@@ -0,0 +1,17 @@
+const newrelic = require('newrelic');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  newrelic.startBackgroundTransaction('helloworld', () => {
+    const txn = newrelic.getTransaction();
+    const name = process.env.NAME || 'World';
+    res.send(`Hello ${name}!`);
+    txn.end();
+  });
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
