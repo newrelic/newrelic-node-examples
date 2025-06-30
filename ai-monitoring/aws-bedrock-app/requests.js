0133f5e729d5b805aaa7d0d1e988d783f1b157da
mrickard <maurice@mauricerickard.com>
2025-06-30 17:16:04 -0400

    chore: Removed unavailable models in Bedrock example, corrected example requests and responses
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- ai-monitoring/aws-bedrock-app/requests.js
+++ ai-monitoring/aws-bedrock-app/requests.js
@@ -9,32 +9,46 @@ module.exports = {
     modelId: 'amazon.titan-embed-text-v1',
     body: { inputText: message },
   }),
-  'anthropic': (message) => ({
-    modelId: 'anthropic.claude-v2',
-    body: {
-      prompt: `\n\nHuman: ${message}\n\nAssistant:`,
-      max_tokens_to_sample: 200,
-    },
-  }),
   'ai21': (message) => ({
     modelId: 'ai21.j2-ultra-v1',
     body: { prompt: message },
   }),
-  'cohere': (message) => ({
-    modelId: 'cohere.command-text-v14',
-    body: { prompt: message },
-  }),
-  'cohere-embed': (message) => ({
-    modelId: 'cohere.embed-english-v3',
-    body: { texts: message.split(' '), input_type: 'search_document' },
-  }),
-  'llama2': (message) => ({
-    modelId: 'meta.llama2-13b-chat-v1',
-    body: { prompt: message },
+  'claude3': (message) => ({
+    modelId: 'us.anthropic.claude-3-haiku-20240307-v1:0',
+    body: {
+      'anthropic_version': "bedrock-2023-05-31",
+      'max_tokens': 256,
+      messages: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'text',
+              text: message
+            }
+          ]
+        }
+      ]
+    },
   }),
   'llama3': (message) => ({
-    modelId: 'meta.llama3-8b-instruct-v1:0',
+    modelId: 'us.meta.llama3-3-70b-instruct-v1:0',
     body: { prompt: message },
+  }),
+  'nova-micro': (message) => ({
+    modelId: 'us.amazon.nova-micro-v1:0',
+    body: {
+      messages: [
+        {
+          role: 'user',
+          content: [
+            {
+              text: message
+            }
+          ]
+        }
+      ]
+    }
   }),
   'amazon-invalid': (message) => ({
     modelId: 'amazon.titan-text-express-v1',
