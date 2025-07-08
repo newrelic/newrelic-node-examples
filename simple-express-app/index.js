50f1a43c176075c2bd84a9e91268e15378b9e360
Amy Chisholm <achisholm@newrelic.com>
2025-07-08 08:03:49 -0700

    temp

--- simple-express-app/index.js
+++ simple-express-app/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const { PORT = 3000 } = process.env
 const lib = require('@aws-sdk/client-sns')
+const { Client } = require('pg')
+const Cursor = require('pg-cursor')
+const newrelic = require('newrelic')
 const sns = new lib.SNSClient()
 
 app.get('/named-route', (req, res) => {
@@ -12,7 +15,36 @@ app.get('/sns', async (req, res) => {
   const cmd = new lib.ListTopicsCommand({})
   const result = await sns.send(cmd)
   res.send(result)
+})
 
+app.get('/pg-cursor', async (req, res) => {
+  const client = new Client({
+    connectionString: 'postgres://postgres:postgres@localhost:5432/postgres'
+  })
+  const TABLE = 'testTable-post'
+  const TABLE_PREPARED = '"' + TABLE + '"'
+  const PK = 'pk_column'
+  const COL = 'test_column'
+  try {
+    await client.connect()
+    await client.query(`CREATE TABLE IF NOT EXISTS ${TABLE_PREPARED} (${PK} SERIAL PRIMARY KEY, ${COL} TEXT)`)
+    await seed()
+
+    const cursor = client.query(new Cursor(`select * from ${TABLE_PREPARED}`))
+    const result = await cursor.read(10)
+    cursor.close()
+    res.json(result)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Error fetching data')
+  } finally {
+    await client.end()
+  }
+  async function seed() {
+    for (let i = 0; i < 10; i += 1) {
+      await client.query(`insert into ${TABLE_PREPARED} (${PK}, ${COL}) values($1, $2)`, [i, `item ${i}`])
+    }
+  }
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- simple-express-app/newrelic.js
+++ simple-express-app/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
+    level: 'trace'
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
