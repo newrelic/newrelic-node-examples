7a8475cd5d0aa544d1372dd69a3cef514cf83d24
Amy Chisholm <achisholm@newrelic.com>
2025-07-17 14:17:34 -0700

    add transaction

--- ai-monitoring/mcp-sdk/client.js
+++ ai-monitoring/mcp-sdk/client.js
@@ -1,23 +1,29 @@
+const newrelic = require('newrelic');
 const { MCPServerStdio, Agent, run } = require("@openai/agents");
 
 async function main() {
-    const mcpServer = new MCPServerStdio({
-        name: "weather",
-        fullCommand: "npm run server"
+    newrelic.startBackgroundTransaction('mcp-client', async () => {
+        const txn = newrelic.getTransaction();
+        const mcpServer = new MCPServerStdio({
+            name: "weather",
+            stdio: 'inherit',
+            fullCommand: "npm run server:debug"
+        })
+        await mcpServer.connect();
+        const agent = new Agent({
+            name: "WeatherAgent",
+            instructions: "You are a weather information agent. Answer questions about the weather.",
+            mcpServers: [mcpServer],
+        });
+
+        const result = await run(agent, "What is the weather like in San Francisco?");
+
+        console.log("Agent response:", result.finalOutput);
+        txn.end();
+        newrelic.shutdown({collectPendingData: true}, () => {
+            process.exit(0);
+        });
     })
-    await mcpServer.connect();
-    const agent = new Agent({
-        name: "WeatherAgent",
-        instructions: "You are a weather information agent. Answer questions about the weather.",
-        mcpServers: [mcpServer],
-    });
-
-    const result = await run(agent, "What is the weather like in San Francisco?");
-
-    console.log("Agent response:", result.finalOutput);
 }
 
-main().catch((error) => {
-    console.error("Fatal error in main():", error);
-    process.exit(1);
-});
+main();
