31e5a872e9c0a49c8e04abdaecf5cfffe7c5984f
Amy Chisholm <achisholm@newrelic.com>
2025-09-23 15:27:29 -0700

    Change Converse model and id and default region

--- ai-monitoring/aws-bedrock-app/index.js
+++ ai-monitoring/aws-bedrock-app/index.js
@@ -15,13 +15,13 @@ const {
   ConverseStreamCommand
 } = require('@aws-sdk/client-bedrock-runtime')
 
-const CONVERSE_API_MODEL_ID = 'anthropic.claude-instant-v1'
+const CONVERSE_API_MODEL_ID = 'us.amazon.nova-micro-v1:0'
 
 const requests = require('./requests')
 const responses = new Map()
 
 const client = new BedrockRuntimeClient({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.AWS_REGION || 'us-west-2',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
